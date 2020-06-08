e7ab6e6cefd217051f71afb51ea4afa5309680b8
UX404 <ux404.nf@gmail.com>
2020-06-08 22:13:43 +0800

    Add files via upload

---
+++ #606 Construct String from Binary Tree.py
@@ -0,0 +1,58 @@
+'''
+You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
+
+The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+
+Example 1:
+Input: Binary tree: [1,2,3,4]
+       1
+     /   \
+    2     3
+   /
+  4
+
+Output: "1(2(4))(3)"
+
+Explanation: Originallay it needs to be "1(2(4)())(3()())",
+but you need to omit all the unnecessary empty parenthesis pairs.
+And it will be "1(2(4))(3)".
+Example 2:
+Input: Binary tree: [1,2,3,null,4]
+       1
+     /   \
+    2     3
+     \
+      4
+
+Output: "1(2()(4))(3)"
+
+Explanation: Almost the same as the first example,
+except we can't omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def tree2str(self, t: TreeNode) -> str:
+        def preorder(node):
+            if not node: return ""
+            sub = ""
+            sub += str(node.val)
+            temp1 = preorder(node.left)
+            temp2 = preorder(node.right)
+            if temp1:
+                sub += '(' + temp1 + ')'
+                if temp2:
+                    sub += '(' + temp2 + ')'
+            else:
+                if temp2:
+                    sub += '()(' + temp2 + ')'
+            return sub
+
+        result = preorder(t)
+        return result
