2f749f9058e0800e874e6cd4cb14ec238a8f35f4
UX404 <ux404.nf@gmail.com>
2020-07-17 00:03:01 +0800

    Add files via upload

---
+++ #783 Minimum Distance Between BST Nodes.py
@@ -0,0 +1,46 @@
+'''
+Given a Binary Search Tree (BST) with the root node root, return the minimum difference between the values of any two different nodes in the tree.
+
+Example :
+
+Input: root = [4,2,6,1,3,null,null]
+Output: 1
+Explanation:
+Note that root is a TreeNode object, not an array.
+
+The given tree [4,2,6,1,3,null,null] is represented by the following diagram:
+
+          4
+        /   \
+      2      6
+     / \
+    1   3
+
+while the minimum difference in this tree is 1, it occurs between node 1 and node 2, also between node 3 and node 2.
+Note:
+
+The size of the BST will be between 2 and 100.
+The BST is always valid, each node's value is an integer, and each node's value is different.
+This question is the same as 530: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def minDiffInBST(self, root: TreeNode) -> int:
+        nums = []
+        nodes = [root]
+        while nodes:
+            node = nodes.pop()
+            nums.append(node.val)
+            if node.left: nodes.append(node.left)
+            if node.right: nodes.append(node.right)
+        nums.sort()
+        for n in range(len(nums)-1):
+            nums[n] = nums[n+1] - nums[n]
+        return min(nums)
