762ab8d80c90800ec7f84333ff44936b27647f17
UX404 <ux404.nf@gmail.com>
2020-09-24 23:51:06 +0800

    Add files via upload

---
+++ #1037 Valid Boomerang.py
@@ -0,0 +1,37 @@
+'''
+A boomerang is a set of 3 points that are all distinct and not in a straight line.
+
+Given a list of three points in the plane, return whether these points are a boomerang.
+
+
+
+Example 1:
+
+Input: [[1,1],[2,3],[3,2]]
+Output: true
+Example 2:
+
+Input: [[1,1],[2,2],[3,3]]
+Output: false
+
+
+Note:
+
+points.length == 3
+points[i].length == 2
+0 <= points[i][j] <= 100
+'''
+
+
+# 1 Vector
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        dx1 = points[0][0] - points[1][0]
+        dx2 = points[2][0] - points[1][0]
+        dy1 = points[0][1] - points[1][1]
+        dy2 = points[2][1] - points[1][1]
+        return (dx1 * dy2) != (dy1 * dx2)
+
+
+# 2 Determinant
+# S != 0
